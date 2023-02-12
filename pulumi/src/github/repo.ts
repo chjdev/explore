683507da9bf2c8e0e72123c7d65df538b8b413fa
Christian Junker <chjdev@users.noreply.github.com>
2023-02-12 17:00:35 +0100

    fix: release

--- pulumi/src/github/repo.ts
+++ pulumi/src/github/repo.ts
@@ -6,7 +6,7 @@ export const repo = new github.Repository(
     name: "explore",
     visibility: "public",
     allowRebaseMerge: true,
-    allowSquashMerge: true,
+    allowSquashMerge: false,
     allowMergeCommit: false,
     allowUpdateBranch: true,
     deleteBranchOnMerge: true,
